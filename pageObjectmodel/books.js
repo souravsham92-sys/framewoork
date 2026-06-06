2c57c87838943a35a85a35a0b3b057fc9eec8e68
souravsham92-sys <sourav.sham92@gmail.com>
2026-06-06 19:55:59 +0530

    modify

--- pageObjectmodel/books.js
+++ pageObjectmodel/books.js
@@ -1,42 +1,42 @@
-import { Dropdown } from "../utils/util";
-import { expect } from "@playwright/test";
-import registration from '../test data/registration.json'
-export class BooksPage{
-    constructor(page){
-        this.page = page;
+// import { Dropdown } from "../utils/util";
+// import { expect } from "@playwright/test";
+// import registration from '../test data/registration.json'
+// export class BooksPage{
+//     constructor(page){
+//         this.page = page;
 
-        this.booksLink = page.locator("(//a[@href= '/books'])[1]")
-        this.nextButton = page.locator('//li[@class="next-page"]')
-        this.scienceBook = page.locator('//a[text()="Science"]')
-        this.addReviewLink = page.locator('//a[text()="Add your review"]')
-        this.reviewTitle = page.locator('#AddProductReview_Title')
-        this.reviewText = page.locator('#AddProductReview_ReviewText')
-        this.rating = page.locator('#addproductrating_4')
-        this.submitButton = page.locator('//input[@class="button-1 write-product-review-button"]')
+//         this.booksLink = page.locator("(//a[@href= '/books'])[1]")
+//         this.nextButton = page.locator('//li[@class="next-page"]')
+//         this.scienceBook = page.locator('//a[text()="Science"]')
+//         this.addReviewLink = page.locator('//a[text()="Add your review"]')
+//         this.reviewTitle = page.locator('#AddProductReview_Title')
+//         this.reviewText = page.locator('#AddProductReview_ReviewText')
+//         this.rating = page.locator('#addproductrating_4')
+//         this.submitButton = page.locator('//input[@class="button-1 write-product-review-button"]')
         
-        this.productItem = page.locator('.product-item')
-        this.reviewResult = page.locator('//div[@class="result"]')
-        this.logout= page.locator('//a[@class="ico-logout"]')
+//         this.productItem = page.locator('.product-item')
+//         this.reviewResult = page.locator('//div[@class="result"]')
+//         this.logout= page.locator('//a[@class="ico-logout"]')
 
-    }
+//     }
 
-    async sortBook(){
-        await this.booksLink.click()
-        await Dropdown(this.page,'#products-orderby',"Name: A to Z")
-        await Dropdown(this.page, '#products-pagesize',"4")
-        await expect(this.productItem).toHaveCount(4);
+//     async sortBook(){
+//         await this.booksLink.click()
+//         await Dropdown(this.page,'#products-orderby',"Name: A to Z")
+//         await Dropdown(this.page, '#products-pagesize',"4")
+//         await expect(this.productItem).toHaveCount(4);
         
-await this.nextButton.click()
- await this.scienceBook.click()
-    }
+// await this.nextButton.click()
+//  await this.scienceBook.click()
+//     }
 
-    async addReview(){
-        await this.addReviewLink.click()
-        await this.reviewTitle.fill(registration.reviewtitle)
-        await this.reviewText.fill(registration.reviewtext)
-        await this.rating.check()
-        await this.submitButton.click()
-        await expect(this.reviewResult).toContainText('Product review is successfully added')
-        await this.logout.click();
-    }
-}
+//     async addReview(){
+//         await this.addReviewLink.click()
+//         await this.reviewTitle.fill(registration.reviewtitle)
+//         await this.reviewText.fill(registration.reviewtext)
+//         await this.rating.check()
+//         await this.submitButton.click()
+//         await expect(this.reviewResult).toContainText('Product review is successfully added')
+//         await this.logout.click();
+//     }
+// }
--- tests/tc03.spec.js
+++ tests/tc03.spec.js
@@ -1,35 +1,35 @@
-import{expect, test}from '@playwright/test'
-import tc03 from "../test data/tc03.json"
-import { log } from 'node:console'
-test('tc03',async ({page}) => {
-    await page.goto(tc03.url)
-    await page.locator('//a[@class="ico-login"]').click()
-    await page.locator('#Email').fill(tc03.email)
-    await page.locator('#Password').fill(tc03.password)
-    await page.locator('//input[@class="button-1 login-button"]').click()
-    await page.locator('//input[@class="search-box-text ui-autocomplete-input"]').fill(tc03.search)
-    await page.keyboard.press('Enter')
-    await page.locator('//a[@title="Show details for Blue Jeans"]').click()
-    await page.locator('//input[@class="button-2 email-a-friend-button"]').click()
-    await page.locator('#FriendEmail').fill(tc03.friends_email)
-     await page.locator('#PersonalMessage').fill(tc03.prsnl_msg)
-    await page.locator('//input[@name="send-email"]').click()
-    const result=page.locator('//div[@class="result"]')
-    await expect(result).toContainText("Your message has been sent")
-    const logout= page.locator('//a[@href="/logout"]')
-    await logout.waitFor({state:'visible'})
-        await logout.click()
-    await page.locator('//a[text()="$25 Virtual Gift Card"]').click()
-    await page.locator('//div[@class="email-a-friend"]').click()
-    await page.locator('//input[@class="friend-email"]').fill(tc03.friends_email)
-    await page.locator('//input[@class="your-email"]').fill(tc03.email)
-    await page.locator('//input[@class="button-1 send-email-a-friend-button"]').click()
-    let res= page.locator('//li[text()= "Only registered customers can use email a friend feature"]')
-    await expect(res).toContainText('"Only registered customers can use email a friend feature"')
+// import{expect, test}from '@playwright/test'
+// import tc03 from "../test data/tc03.json"
+// import { log } from 'node:console'
+// test('tc03',async ({page}) => {
+//     await page.goto(tc03.url)
+//     await page.locator('//a[@class="ico-login"]').click()
+//     await page.locator('#Email').fill(tc03.email)
+//     await page.locator('#Password').fill(tc03.password)
+//     await page.locator('//input[@class="button-1 login-button"]').click()
+//     await page.locator('//input[@class="search-box-text ui-autocomplete-input"]').fill(tc03.search)
+//     await page.keyboard.press('Enter')
+//     await page.locator('//a[@title="Show details for Blue Jeans"]').click()
+//     await page.locator('//input[@class="button-2 email-a-friend-button"]').click()
+//     await page.locator('#FriendEmail').fill(tc03.friends_email)
+//      await page.locator('#PersonalMessage').fill(tc03.prsnl_msg)
+//     await page.locator('//input[@name="send-email"]').click()
+//     const result=page.locator('//div[@class="result"]')
+//     await expect(result).toContainText("Your message has been sent")
+//     const logout= page.locator('//a[@href="/logout"]')
+//     await logout.waitFor({state:'visible'})
+//         await logout.click()
+//     await page.locator('//a[text()="$25 Virtual Gift Card"]').click()
+//     await page.locator('//div[@class="email-a-friend"]').click()
+//     await page.locator('//input[@class="friend-email"]').fill(tc03.friends_email)
+//     await page.locator('//input[@class="your-email"]').fill(tc03.email)
+//     await page.locator('//input[@class="button-1 send-email-a-friend-button"]').click()
+//     let res= page.locator('//li[text()= "Only registered customers can use email a friend feature"]')
+//     await expect(res).toContainText("Only registered customers can use email a friend feature"')
 
 
 
 
 
     
-})
+// })
